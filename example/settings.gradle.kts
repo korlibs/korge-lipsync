4d04b182f43d7b1361b99e14686bbe7387c652f0
Carlos Ballesteros Velasco <soywiz@gmail.com>
2023-02-02 13:07:37 +0100

    Initial commit

---
+++ example/settings.gradle.kts
@@ -0,0 +1,10 @@
+pluginManagement { repositories {  mavenLocal(); mavenCentral(); google(); gradlePluginPortal()  }  }
+
+plugins {
+    //id("com.soywiz.kproject.settings") version "0.0.1-SNAPSHOT"
+    id("com.soywiz.kproject.settings") version "0.1.2"
+}
+
+rootProject.name = "korge-kproject-template-example"
+
+kproject("./deps")
